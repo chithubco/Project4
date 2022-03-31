019cec28249e8821ef6456b18bfd11cc3cdce3d8
Sirmikky <michael.agbogo@echithub.com>
2022-03-31 14:28:46 +0100

    Property Animator for button

--- app/src/main/java/com/echithub/project4/MainFragment.kt
+++ app/src/main/java/com/echithub/project4/MainFragment.kt
@@ -1,6 +1,9 @@
 package com.echithub.project4
 
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
 import android.app.DownloadManager
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -26,6 +29,7 @@ class MainFragment : Fragment() {
 
     private var downloadStarted = false
     private var downloadLink: String = ""
+    private var downloadCompleted: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +41,7 @@ class MainFragment : Fragment() {
         binding.btnDownload.setOnClickListener {
             binding.btnDownload.text = "DOWNLOAD IN PROGRESS...."
             download(downloadLink, FILE_NAME)
+            runAnimation()
         }
 
         binding.rgSelectDownloadSource.setOnCheckedChangeListener { group, checkedId ->
@@ -50,6 +55,31 @@ class MainFragment : Fragment() {
         setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    private fun runAnimation(){
+//        val animator = ObjectAnimator.ofFloat(binding.btnDownload,View.ALPHA,0.2f)
+        val animator = ObjectAnimator.ofArgb(binding.btnDownload,"backgroundColor", Color.BLUE, Color.YELLOW)
+        animator.duration = 1000
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                animator.repeatCount = 2
+                animator.repeatMode = ObjectAnimator.RESTART
+            }
+            override fun onAnimationEnd(animation: Animator?) {
+
+                if (!downloadCompleted) {
+                    animator.start()
+
+                }else{
+                    animator.repeatCount = 0
+                    animator.cancel()
+                    binding.btnDownload.setBackgroundColor(Color.GREEN)
+
+                }
+            }
+        })
+        animator.start()
     }
 
     private fun download(url: String,fileName: String) {
@@ -78,7 +108,7 @@ class MainFragment : Fragment() {
         val br = object : BroadcastReceiver(){
             override fun onReceive(context: Context?, intent: Intent?) {
                 Log.i("Fragment Receiver",intent.toString())
-
+                downloadCompleted = true
                 binding.btnDownload.text = "DOWNLOAD COMPETED"
                 binding.btnDownload.setBackgroundColor(Color.GREEN)
             }
